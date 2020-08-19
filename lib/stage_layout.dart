fed1526fcef2972d6df2fa3c8a4a4d98e0cc8b4b
moyuqin <yqmo@liangzhicn.com>
2020-08-19 18:46:30 +0800

    commit

--- lib/stage_layout.dart
+++ lib/stage_layout.dart
@@ -32,22 +32,41 @@ class StageRender extends RenderBox
     prepareRenderBoxes = List();
     readiedRenderBoxes = List();
     prepareRenderBoxes = getChildrenAsList();
-    BoxConstraints nonPositionedConstraints;
-    nonPositionedConstraints = constraints.loosen();
-    bool hasNonRelativeChildren = false;
     size = constraints.biggest;
+    layoutChildren();
+  }
 
-    prepareRenderBoxes.forEach((child) {
+  void layoutChildren() {
+    bool hasNonRelativeChildren = false;
+    int hadLayoutChildCount = 0;
+    for (int i = 0; i < prepareRenderBoxes.length; i++) {
+      RenderBox child = prepareRenderBoxes[i];
       final StageParentData childParentData = child.parentData;
       if (!childParentData.isRelative()) {
         hasNonRelativeChildren = true;
       }
       if (!hasNonRelativeChildren) {
-        layoutForRelativeChild(child);
+        if (!layoutForRelativeChild(child)) {
+          continue;
+        }
       }
-      child.layout(nonPositionedConstraints, parentUsesSize: true);
       readiedRenderBoxes.add(child);
-    });
+      prepareRenderBoxes.remove(child);
+      i--;
+      hadLayoutChildCount++;
+      child.layout(constraints.loosen(), parentUsesSize: true);
+    }
+    if (prepareRenderBoxes.isNotEmpty) {
+      if (hadLayoutChildCount != 0) {
+        layoutChildren();
+      } else {
+        var unLayoutChildTag = "";
+        prepareRenderBoxes.forEach((element) {
+          unLayoutChildTag += (element.parentData as StageParentData).tag + " ";
+        });
+        assert(false, 'Tag $unLayoutChildTag can not sure layout.');
+      }
+    }
   }
 
   double measureDx(RenderBox child, StageParentData childParentData) {
@@ -55,30 +74,28 @@ class StageRender extends RenderBox
     double xEndPoint;
     double dx;
 
-    var beLeftPoint = measurePoint(childParentData.beLeft, child, childParentData, false, true, false);
-    var toLeftPoint = measurePoint(childParentData.toLeft, child, childParentData, false, true, true);
-    var beRightPoint = measurePoint(childParentData.beRight, child, childParentData, false, false, false);
-    var toRightPoint = measurePoint(childParentData.toRight, child, childParentData, false, false, true);
+    var beLeftPoint = measurePoint(
+        childParentData.beLeft, child, childParentData, false, true, false);
+    var toLeftPoint = measurePoint(
+        childParentData.toLeft, child, childParentData, false, true, true);
+    var beRightPoint = measurePoint(
+        childParentData.beRight, child, childParentData, false, false, false);
+    var toRightPoint = measurePoint(
+        childParentData.toRight, child, childParentData, false, false, true);
 
-    print('$beLeftPoint $toLeftPoint $beRightPoint $toRightPoint');
-
-    [beLeftPoint,toLeftPoint,beRightPoint,toRightPoint].forEach((element) {
-      if(element != null){
-        if(xStartPoint == null)
+    [beLeftPoint, toLeftPoint, beRightPoint, toRightPoint].forEach((element) {
+      if (element != null) {
+        if (xStartPoint == null)
           xStartPoint = element;
         else
           xEndPoint = element;
       }
     });
 
-    print("$xStartPoint  $xEndPoint");
-
     if (xStartPoint == null && xEndPoint == null) {
       dx = 0.0;
-    } else if (xStartPoint == null) {
-      dx = xEndPoint - child.getMaxIntrinsicWidth(0);
     } else if (xEndPoint == null) {
-      dx = xStartPoint;
+      dx = xStartPoint - child.getMaxIntrinsicWidth(0) / 2;
     } else {
       dx = (xEndPoint - xStartPoint).abs() / 2 +
           math.min(xStartPoint, xEndPoint) -
@@ -93,14 +110,18 @@ class StageRender extends RenderBox
     double yEndPoint;
     double dy;
 
-    var beTopPoint = measurePoint(childParentData.beTop, child, childParentData, true, true, false);
-    var toTopPoint = measurePoint(childParentData.toTop, child, childParentData, true, true, true);
-    var beBottomPoint = measurePoint(childParentData.beBottom, child, childParentData, true, false, false);
-    var toBottomPoint = measurePoint(childParentData.toBottom, child, childParentData, true, false, true);
+    var beTopPoint = measurePoint(
+        childParentData.beTop, child, childParentData, true, true, false);
+    var toTopPoint = measurePoint(
+        childParentData.toTop, child, childParentData, true, true, true);
+    var beBottomPoint = measurePoint(
+        childParentData.beBottom, child, childParentData, true, false, false);
+    var toBottomPoint = measurePoint(
+        childParentData.toBottom, child, childParentData, true, false, true);
 
-    [beTopPoint,toTopPoint,beBottomPoint,toBottomPoint].forEach((element) {
-      if(element != null){
-        if(yStartPoint == null)
+    [beTopPoint, toTopPoint, beBottomPoint, toBottomPoint].forEach((element) {
+      if (element != null) {
+        if (yStartPoint == null)
           yStartPoint = element;
         else
           yEndPoint = element;
@@ -109,10 +130,8 @@ class StageRender extends RenderBox
 
     if (yStartPoint == null && yEndPoint == null) {
       dy = 0.0;
-    } else if (yStartPoint == null) {
-      dy = yEndPoint - child.getMaxIntrinsicHeight(0);
     } else if (yEndPoint == null) {
-      dy = yStartPoint;
+      dy = yStartPoint - child.getMaxIntrinsicHeight(0) / 2;
     } else {
       dy = (yEndPoint - yStartPoint) / 2 +
           yStartPoint -
@@ -133,16 +152,34 @@ class StageRender extends RenderBox
     if (align != null) {
       if (align == PARENT) {
         point = 0;
-        if (!isStartPoint) {
+        if (isStartPoint) {
+          if (isVertical) {
+            if (isOutSide) {
+              point -= child.getMaxIntrinsicHeight(0) / 2;
+            } else {
+              point += child.getMaxIntrinsicHeight(0) / 2;
+            }
+          } else {
+            if (isOutSide) {
+              point -= child.getMaxIntrinsicWidth(0) / 2;
+            } else {
+              point += child.getMaxIntrinsicWidth(0) / 2;
+            }
+          }
+        } else {
           if (isVertical) {
             point = size.height;
             if (isOutSide) {
-              point += child.getMaxIntrinsicHeight(0);
+              point += child.getMaxIntrinsicHeight(0) / 2;
+            } else {
+              point -= child.getMaxIntrinsicHeight(0) / 2;
             }
           } else {
             point = size.width;
             if (isOutSide) {
-              point += child.getMaxIntrinsicWidth(0);
+              point += child.getMaxIntrinsicWidth(0) / 2;
+            } else {
+              point -= child.getMaxIntrinsicWidth(0) / 2;
             }
           }
         }
@@ -154,24 +191,32 @@ class StageRender extends RenderBox
           point = targetParentData.offset.dy;
           if (isStartPoint) {
             if (isOutSide) {
-              point -= child.getMaxIntrinsicHeight(0);
+              point -= child.getMaxIntrinsicHeight(0) / 2;
+            } else {
+              point += child.getMaxIntrinsicHeight(0) / 2;
             }
           } else {
             point += targetRenderBox.getMaxIntrinsicHeight(0);
-            if(isOutSide){
-              point += child.getMaxIntrinsicHeight(0);
+            if (isOutSide) {
+              point += child.getMaxIntrinsicHeight(0) / 2;
+            } else {
+              point -= child.getMaxIntrinsicHeight(0) / 2;
             }
           }
         } else {
           point = targetParentData.offset.dx;
           if (isStartPoint) {
             if (isOutSide) {
-              point -= child.getMaxIntrinsicWidth(0);
+              point -= child.getMaxIntrinsicWidth(0) / 2;
+            } else {
+              point += child.getMaxIntrinsicWidth(0) / 2;
             }
           } else {
             point += targetRenderBox.getMaxIntrinsicWidth(0);
-            if(isOutSide){
-              point += child.getMaxIntrinsicWidth(0);
+            if (isOutSide) {
+              point += child.getMaxIntrinsicWidth(0) / 2;
+            } else {
+              point -= child.getMaxIntrinsicWidth(0) / 2;
             }
           }
         }
@@ -180,7 +225,7 @@ class StageRender extends RenderBox
     return point;
   }
 
-  void layoutForRelativeChild(RenderBox child) {
+  bool layoutForRelativeChild(RenderBox child) {
     final StageParentData childParentData = child.parentData;
     var needLayout = true;
     var tags = childParentData.getRelyChildrenTags();
@@ -192,9 +237,28 @@ class StageRender extends RenderBox
     if (needLayout) {
       assert(childParentData.isMeasureAble(),
           'There can only be two or less constraints in the same direction');
+
       childParentData.offset = Offset(
-          measureDx(child, childParentData), measureDy(child, childParentData));
+          measureDx(child, childParentData) + getOffsetX(childParentData.margin), measureDy(child, childParentData) + getOffsetY(childParentData.margin));
+      return true;
     }
+    return false;
+  }
+
+  double getOffsetX(EdgeInsets margin){
+    var x = 0.0;
+    if(margin != null){
+        x = margin.left - margin.right;
+    }
+    return x;
+  }
+
+  double getOffsetY(EdgeInsets margin){
+    var y = 0.0;
+    if(margin != null){
+        y = margin.top - margin.bottom;
+    }
+    return y;
   }
 
   RenderBox getRenderBoxByTag(String tag) {
@@ -210,6 +274,61 @@ class StageRender extends RenderBox
   @override
   void paint(PaintingContext context, Offset offset) {
     defaultPaint(context, offset);
+  }
+}
+
+class RelativeWidget extends StatelessWidget {
+  final Widget child;
+  final Key key;
+  final String tag;
+  final String toTop;
+  final String toBottom;
+  final String toLeft;
+  final String toRight;
+  final String beTop;
+  final String beBottom;
+  final String beLeft;
+  final String beRight;
+  final EdgeInsets padding;
+
+  const RelativeWidget(this.tag,
+      {this.toTop,
+      this.toBottom,
+      this.toLeft,
+      this.toRight,
+      this.beTop,
+      this.beBottom,
+      this.beLeft,
+      this.beRight,
+      this.padding,
+      this.child,
+      this.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (padding != null)
+      return Relative(tag,
+          toTop: toTop,
+          toBottom: toBottom,
+          toLeft: toLeft,
+          toRight: toRight,
+          beTop: beTop,
+          beBottom: beBottom,
+          beLeft: beLeft,
+          beRight: beRight,
+          child: Padding(padding: padding, child: child),
+          key: key);
+    return Relative(tag,
+        toTop: toTop,
+        toBottom: toBottom,
+        toLeft: toLeft,
+        toRight: toRight,
+        beTop: beTop,
+        beBottom: beBottom,
+        beLeft: beLeft,
+        beRight: beRight,
+        child: child,
+        key: key);
   }
 }
 
@@ -246,6 +365,7 @@ class Relative extends ParentDataWidget<StageParentData> {
     final StageParentData parentData =
         renderObject.parentData as StageParentData;
     parentData.tag = tag;
+    parentData.margin = margin;
     parentData.toTop = toTop;
     parentData.toBottom = toBottom;
     parentData.toLeft = toLeft;
